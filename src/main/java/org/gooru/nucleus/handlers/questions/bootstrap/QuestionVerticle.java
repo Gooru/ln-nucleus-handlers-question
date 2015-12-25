eae804b6e308f426768c4ad333fb661fec828fbe
Ashish Mishra <ashish@gooru.org>
2015-12-25 13:57:25 +0530

    Initial repo creation

---
+++ src/main/java/org/gooru/nucleus/handlers/questions/bootstrap/QuestionVerticle.java
@@ -0,0 +1,9 @@
+package org.gooru.nucleus.handlers.questions.bootstrap;
+
+import io.vertx.core.AbstractVerticle;
+
+/**
+ * Created by ashish on 25/12/15.
+ */
+public class QuestionVerticle extends AbstractVerticle {
+}
