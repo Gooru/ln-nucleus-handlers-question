67731191cfd8a2569fd3184ae75d4c979c58c4e4
Ashish Mishra <ashish@gooru.org>
2016-01-11 11:31:16 +0530

    Create entity for question

---
+++ src/main/java/org/gooru/nucleus/handlers/questions/processors/repositories/activejdbc/entities/AJEntityQuestion.java
@@ -0,0 +1,11 @@
+package org.gooru.nucleus.handlers.questions.processors.repositories.activejdbc.entities;
+
+import org.javalite.activejdbc.Model;
+import org.javalite.activejdbc.annotations.Table;
+
+/**
+ * Created by ashish on 11/1/16.
+ */
+@Table("content")
+public class AJEntityQuestion extends Model {
+}
